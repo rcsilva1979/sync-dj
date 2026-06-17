1eeca6b167102374ee1bba03573768a640474482
rodri <rodrigocaldassilva@gmail.com>
2026-06-17 10:18:25 -0300

    Nova funcionalidade - Discovery song

--- constants.py
+++ constants.py
@@ -4,7 +4,7 @@ import sys
 IS_WIN = sys.platform.startswith('win')
 IS_MAC = sys.platform == 'darwin'
 
-VERSAO_ATUAL = "v1.1.0"
+VERSAO_ATUAL = "v1.2.0"
 APP_NAME = "Sync DJ Tools Suite"
 
 # --- ESTILOS GLOBAIS ---
