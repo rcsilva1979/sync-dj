667db58226418337f701ebb7fa9b7b12652c3168
rodri <rodrigocaldassilva@gmail.com>
2026-07-29 19:22:02 -0300

    Ajustes de segurança

--- constants.py
+++ constants.py
@@ -4,7 +4,7 @@ import sys
 IS_WIN = sys.platform.startswith('win')
 IS_MAC = sys.platform == 'darwin'
 
-VERSAO_ATUAL = "v2.0.3"
+VERSAO_ATUAL = "v2.0.4"
 APP_NAME = "Sync DJ Tools Suite"
 
 # --- ESTILOS GLOBAIS ---
